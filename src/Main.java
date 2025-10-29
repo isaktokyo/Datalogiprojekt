3c08e974d537dbf99c6bb25e87b20da00cd2b696
isaktokyo <isakreite@icloud.com>
2025-10-29 11:43:01 +0100

    amal og isak gruppemøde onsdag 22 oktober

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import static java.lang.reflect.Array.set;
 
 public class Main {
 
@@ -12,16 +15,35 @@ public class Main {
         List<String> lines = indlaesData.getSunSignFromDB(dbPath);
         DefaultDict<Integer, List<Integer>> item_counts =
                 new DefaultDict<Integer, List<Integer>>(ArrayList.class); // dette er en python metode,
-        // som vi har overført til java. vi har lånt det.
-        item_counts.get("Væren").add(1);
-        item_counts.get("Løven").add(1);
-        item_counts.get("Jomfru").add(1);
+        // Som vi har overført til java. Vi har lånt det.
+        DefaultDict<Integer, List<Integer>> pair_counts =
+                new DefaultDict<Integer, List<Integer>>(ArrayList.class);
+      //  item_counts.get("Væren").add(1);
+
+        // first pass
+        for (String line : lines) {//find candidate items
+            for(String item: line.split(",")){
+                item_counts.get(item).add(1);
+            }
+          /*  String sign = TilStjernetegn1.findStjernetegn(line);
+            System.out.println(line + " → " + sign);
+            System.out.println(item_counts);*/
+        }
+        Object frequent_items = set();
+        for(){
+
+        }
+
+
+
+
 
 
         System.out.println(item_counts);
 
         for (String line : lines){
             item_counts.get(1).add(Integer.parseInt(line));
+            System.out.println(item_counts);
         }
     }
     public double supportWall(){ // denne metode skal afgrænse hvor meget support
@@ -32,13 +54,22 @@ public class Main {
     }
     public double findSupport(){return 0;}// returnvalue er bare en placeholder
 
-// metode for at konvertere fødselsdag til horoskop (if -setninger)
+// metode for at konvertere fødselsdag til horoskop (if -sætninger)
 
 // Algoritme - tælle hvor mange stjernetegn der er
 //
     // en main som initierer visualiseringen
     public static void main(String []args){
-       /* Connection conn = null; // dette er bare kode fra forelesningen. det skal fungere at hente fra sqlite filen
+
+
+            String dbPath = "identifier.sqlite";
+            List<String> datoer = indlaesData.getSunSignFromDB(dbPath);
+
+            for (String dato : datoer) {
+                String sign = TilStjernetegn1.findStjernetegn(dato);
+                System.out.println(dato + " → " + sign);
+            }
+       /* Connection conn = null; // dette er bare kode fra forelæsningen. det skal fungere at hente fra sqlite filen
         try {
             String url = "jdbc:sqlite:identifier.sqlite";
             conn = DriverManager.getConnection(url);
@@ -55,4 +86,4 @@ public class Main {
             }
         } */
     }
-}}
+}
