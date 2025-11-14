afa52e6291fc22fc6fa9a51fa4ab6acce5a55702
isaktokyo <isakreite@icloud.com>
2025-11-14 16:29:03 +0100

    Isak metode med amals kommentarer

--- src/Main.java
+++ src/Main.java
@@ -19,27 +19,21 @@ public class Main {
         // Som vi har overført til java. Vi har lånt det.
         DefaultDict<Integer, List<Integer>> pair_counts =
                 new DefaultDict<Integer, List<Integer>>(ArrayList.class);
-        //  item_counts.get("Væren").add(1);
 
         // first pass
         for (String line : lines) {//find candidate items
             for (String item : line.split(",")) {
                 item_counts.get(item).add(1);
             }
-
           /*  String sign = TilStjernetegn1.findStjernetegn(line);
             System.out.println(line + " → " + sign);
             System.out.println(item_counts);*/
         }
-
         /*
         Object frequent_items = set();
-        for(){
-
-        }
+        for(){}
 */
         System.out.println(item_counts);
-
         for (String line : lines) {
             item_counts.get(1).add(Integer.parseInt(line));
             System.out.println(item_counts);
@@ -50,18 +44,13 @@ public class Main {
         // der skal være for en link mellom to datapunkter. F eks: morder - fisk
         double threshold = 0.75;
         return threshold;
-
     }
 
     public double findSupport() {
         return 0;
     }// returnvalue er bare en placeholder
 
-// metode for at konvertere fødselsdag til horoskop (if -sætninger)
-
-// Algoritme - tælle hvor mange stjernetegn der er
-
-    // en main som initierer visualiseringen
+    // en main som initierer visualiseringen og de forskellige metoder.
     public static void main(String[] args) {
         String dbPath = "identifier.sqlite";
         List<String> datoer = indlaesData.getSunSignFromDB(dbPath);
--- src/genererKombs.java
+++ src/genererKombs.java
@@ -1,20 +1,24 @@
 import java.util.*;
 
  public class genererKombs {
-     static void genererKombs(int index, List<String> inputs, int comblength, // geeks4geeks
+     static void genererKombs(int index, List<String> combInst, int comblength, // geeks4geeks
                               Set<ArrayList<String>> resultat, String[] arr) {
-         if (inputs.size() == comblength) {
-             resultat.add(new ArrayList<>(inputs));
+         // index er algoritmens nuværende placering i arr, hvilket er den originale array, eller vores input.
+       // combInst er en instans af en kombination af forskellige Strings der er i gang med at blive bygget. resultat er vores resultat.
+         if (combInst.size() == comblength) { // Når vi har udvalgt nok elementer, gemmes kombinationen og rekursionen stopper.
+             resultat.add(new ArrayList<>(combInst));
              return;
          }
+         // Dette er 'mønsteret' for vores rekursion. mønsteret indeholder 1) valg af arr/element, 2) udforskning af kombinationer,
+         // 3) fravalg af det forrige element til fordel for at finde nye kombi
          for (int i = index; i < arr.length; i++) {
-             inputs.add(arr[i] + ""); // arr er en midlertidig array som holder inputs-Strings
-             genererKombs(i + 1, inputs, comblength, resultat, arr);
-             inputs.remove(inputs.size() - 1);
+             combInst.add(arr[i] + ""); // arr er en midlertidig array som holder combInst-Strings
+             genererKombs(i + 1, combInst, comblength, resultat, arr);
+             combInst.remove(combInst.size() - 1);
          }
      }
+     // Metoden fodre generereKobs med input, og kommunikere med main. Den laver sine egne sets med arraylists, som er resultat1.
      static Set<ArrayList<String>> generer(String arr[], int r) {
-         int n = arr.length;
          Set<ArrayList<String>> resultat1 = new HashSet<>();
          List<String> inputs = new ArrayList<>();
          genererKombs(0, inputs, r, resultat1, arr);
